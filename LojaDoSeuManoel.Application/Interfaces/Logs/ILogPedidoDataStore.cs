aa9f6e4fe783a77830fa371de2e6e9a00e3b5047
Thiago Tescaro <thiagotescarorj@gmail.com>
2024-10-17 19:30:43 -0300

    Ajuste 5

---
+++ LojaDoSeuManoel.Application/Interfaces/Logs/ILogPedidoDataStore.cs
@@ -0,0 +1,14 @@
+﻿using LojaDoSeuManoel.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaDoSeuManoel.Application.Interfaces.Logs
+{
+    public interface ILogPedidoDataStore
+    {
+        Task AddAsync(LogPedidoModel model);
+    }
+}
